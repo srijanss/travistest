8c7cadce64f3b007f48f4721fef32f47b353d73c
Srijan Manandhar <srijan.manandhar@gmail.com>
2018-06-04 22:04:09 +1000

    Removed virtualenv line in travis.yml

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from distutils.core import setup
 setup(
     name='travistest',
     packages=['travistest'],
-    version='0.0.8',
+    version='0.0.9',
     description='Test project to get acquainted with TravisCI',
     url='https://github.com/srijanss/travistest'
 )
