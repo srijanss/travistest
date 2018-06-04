a342349e333833728a6a21827a76361af6f711e3
Srijan Manandhar <srijan.manandhar@gmail.com>
2018-06-04 22:47:05 +1000

    Added pypirc file

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ with open('README.md', 'r') as fh:
 setuptools.setup(
     name='travistest',
     packages=['travistest'],
-    version='0.0.10',
+    version='0.0.11',
     author="Srijan Manandhar",
     author_email="srijan.manandhar@gmail.com",
     description='Test project to get acquainted with TravisCI',
